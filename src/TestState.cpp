b0cd7331d4684c93776d6e80c2c676479bbdde62
theo-walton <theodore_walton@hotmail.com>
2018-04-22 23:38:59 -0700

    removed debug lines ops

--- src/TestState.cpp
+++ src/TestState.cpp
@@ -5,13 +5,11 @@
 TestState::TestState(Engine& e) :
 _engine(e), _window(e.window)
 {
-	std::cout << "test" << std::endl;	
 	_camera.Move(glm::vec3(0, -10, 20));
 	_camera.Rotate(glm::vec3(0, 0, 1), 90);
 	_camera.Rotate(glm::vec3(0, 1, 0), 64);
 	
 	generate_level(_registry, 12, 12);
-	std::cout << "gen" << std::endl;
 
 	_explosion = new ParticleExplosion(1.0f);
 
