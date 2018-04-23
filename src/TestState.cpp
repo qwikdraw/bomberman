6cf46eff4e529f3f3c80bc64854f357997ae722d
theo-walton <theodore_walton@hotmail.com>
2018-04-23 15:23:33 -0700

    made an ai entity to test with

--- src/TestState.cpp
+++ src/TestState.cpp
@@ -11,6 +11,15 @@ _engine(e), _window(e.window)
 	
 	generate_level(_registry, 16, 16);
 
+//generating test ai
+
+	auto ai = _registry.create();
+	_registry.assign<c::Model>(ai, "player", glm::mat4(1));
+	_registry.assign<c::Velocity>(ai);
+	_registry.assign<c::Collide>(ai);
+	_registry.assign<c::AI>(ai, 2.0, 0.5);
+	_registry.assign<c::Position>(ai, glm::vec3(0, 0, 0));
+	
 	_explosion = new ParticleExplosion(2.0f);
 	glClearColor(0.2, 0.25, 0.29, 1.0);
 }
